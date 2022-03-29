b54dac893894b88b7d6ce92bbd7f263616d47ffa
DiogoSimões <32929060+DiogoAlex960@users.noreply.github.com>
2022-03-29 22:15:47 +0100

    1 - Update

---
+++ main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+#include <time.h>
+#include <string>
+
+
+//------------------------------------------------------------
+
+//Estrutura para definir um Processo
+typedef struct Processo{
+	int maquina;
+	time_t t = time(NULL);
+}Processo;
+
+//Lista de n processos
+typedef struct ListadeProcessos{
+	Processo processo;
+	struct ListadeProcessos* proximoprocesso;
+	
+}ListadeProcessos;
+
+
+
+
+//------------------------------------------------------------
+
+
+
+
+//Estrutura para definir uma Operação
+typedef struct Operacao{
+	int operation;
+}Operacao;
+
+//Lista de n operações
+typedef struct ListadeOperacoes{
+	Operacao operation;
+	struct ListadeOperacoes* proximooperacao;
+}ListadeOperacoes;
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------
+
+
+
+//Estrutura para definir um job
+typedef struct Job{
+	string jobid;
+	ListadeOperacoes* operacoes
+}Job;
+
+// Adicionar toda duração do job (DuracaoJob)
+typedef struct DuracaoJob{
+	int job;
+	int duracao;
+}ListadeOperacoes;
+
+
+
+//------------------------------------------------------------
+//-----------------------MAIN---------------------------------
+//------------------------------------------------------------
+
+
+
+int main() {
+	
+	
+	
+	
+	
+	
+}
+
+
+
